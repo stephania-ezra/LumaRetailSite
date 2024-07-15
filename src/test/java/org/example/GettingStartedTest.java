4c0732d6d65a37533a41516be72a7e72c7055424
stephania-ezra <175607859+stephania-ezra@users.noreply.github.com>
2024-07-15 16:33:00 +0530

    First commit

---
+++ src/test/java/org/example/GettingStartedTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+
+public class GettingStartedTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void formSubmitTest() {
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.getTitle();
+        /*Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(d -> revealed.isDisplayed());*/
+        WebElement textArea = driver.findElement(By.name("my-textarea"));
+        WebElement submitButton = driver
+                .findElement(By.xpath("/html/body/main/div/form/div/div[2]/button"));
+        textArea.sendKeys("GettingStarted");
+        submitButton.click();
+
+        WebElement result = driver.findElement(By.xpath("/html/body/main/div/div[1]/div/h1"));
+        assertEquals(result.getText(), "Form submitted");
+    }
+}
