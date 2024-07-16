4fbf7147207a1ff9fff93fd05e4da88642f2f745
stephania-ezra <175607859+stephania-ezra@users.noreply.github.com>
2024-07-16 12:44:41 +0530

    Adding Wishlist

--- src/test/java/org/example/WishlistTest.java
+++ src/test/java/org/example/WishlistTest.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class WishlistTest extends LumaLoginTest {
 
@@ -11,8 +15,23 @@ public class WishlistTest extends LumaLoginTest {
     }
 
     @Test
-    public void wishList() {
+    public void wishlist() {
+        login();
 
-        // TODO
+        // Dropdown in Profile
+        WebElement signOutToggleButton = driver
+                .findElement(By.xpath("/html/body/div[2]/header/div[1]/div/ul/li[2]/span/button"));
+        signOutToggleButton.click();
+
+
+        WebElement myWishList = driver
+                .findElement(By.xpath("/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[2]/a"));
+        myWishList.click();
+
+        // Message in Wishlist
+        WebElement message = driver
+                .findElement(By.xpath("/html/body/div[2]/main/div[2]/div[1]/form/div[1]/span"));
+        System.out.println("message " + message.getText());
+        assertEquals(message.getText(), "You have no items in your wish list.");
     }
 }
