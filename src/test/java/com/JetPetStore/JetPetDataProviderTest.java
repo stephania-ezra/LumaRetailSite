5ae6b4c7a4b013766a27b1b9ca86d115e62ddceb
stephania-ezra <175607859+stephania-ezra@users.noreply.github.com>
2024-10-31 12:33:51 +0530

    Data Providers concept applied for JetPetStore site

--- src/test/java/com/JetPetStore/JetPetDataProviderTest.java
+++ src/test/java/com/JetPetStore/JetPetDataProviderTest.java
@@ -11,10 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 @Listeners({CustomListener.class})
 public class JetPetDataProviderTest extends BaseUtilTest {
@@ -33,134 +30,104 @@ public class JetPetDataProviderTest extends BaseUtilTest {
         but.tearDown(driver);
     }
 
-    @Test(priority = 0)
-    public void LaunchApp() {
-        but.stopTheFlow(3);
+    @Test(dataProvider = "JetPetLoginData")
+    public void JetPetCreateUser(String UserId, String NewPassword, String RepeatPassword
+            , String FirstName, String LastName, String Email
+            , String Phone, String Address1, String Address2
+            , String City, String State, String Zip
+            , String Country, String Language, String Category) {
+
         log.info("Step1 : Launching URL");
         driver.manage().window().maximize();
         driver.get("https://petstore.octoperf.com/actions/Catalog.action");
-        but.stopTheFlow(3);
-    }
 
-    @Test(priority = 1)
-    public void ClickSignIn() {
-        but.stopTheFlow(3);
         log.info("Step 2: On Clicking SignIn");
         WebElement SignInElement = driver.findElement(By.xpath("//*[@id=\"MenuContent\"]/a[2]"));
         SignInElement.click();
-        but.stopTheFlow(3);
-    }
 
-    @Test(priority = 2)
-    public void ClickingRegisterNow() {
-        but.stopTheFlow(3);
         log.info("Step 3 : Clicking Register Now");
         WebElement RegisterNowElement = driver.findElement(By.xpath("//*[@id=\"Catalog\"]/a"));
         RegisterNowElement.click();
         but.stopTheFlow(3);
-    }
 
-    @Test(priority = 3)
-    public void UserInformationTitle() {
-        but.stopTheFlow(3);
         log.info("Step 4: User Information");
         WebElement UserInformationTitleElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/h3[1]"));
         Assert.assertTrue(UserInformationTitleElement.isDisplayed());
         log.info("User Information:{}", UserInformationTitleElement.getText());
-        but.stopTheFlow(3);
-    }
 
-    @Test(priority = 4)
-    public void UserInformationDetails() {
-        but.stopTheFlow(3);
         log.info("Step 5 : Displaying User Information Details");
-        but.stopTheFlow(3);
+
         WebElement UserIdElement = driver.findElement(By.name("username"));
-        UserIdElement.sendKeys("Ethan");
-        but.stopTheFlow(3);
+        UserIdElement.sendKeys(UserId);
 
         WebElement NewPasswordElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[1]/tbody/tr[2]/td[2]/input"));
-        NewPasswordElement.sendKeys("123ABC");
+        NewPasswordElement.sendKeys(NewPassword);
 
         WebElement RepeatPasswordElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[1]/tbody/tr[3]/td[2]/input"));
-        RepeatPasswordElement.sendKeys("123ABC");
-        but.stopTheFlow(3);
-    }
+        RepeatPasswordElement.sendKeys(RepeatPassword);
 
-    @Test(priority = 5)
-    public void AccountInformationTitle() {
-        but.stopTheFlow(3);
         log.info("Step 5: Account Information");
         WebElement AccountInformationTitleElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/h3[2]"));
         Assert.assertTrue(AccountInformationTitleElement.isDisplayed());
         log.info("Account Information Title :{}", AccountInformationTitleElement.getText());
-        but.stopTheFlow(3);
-    }
 
-    @Test(priority = 6)
-    public void AccountInformationDetails() {
-        but.stopTheFlow(3);
         log.info("Step 6 : Displaying Account Information Details");
         but.stopTheFlow(3);
 
         WebElement FirstNameElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[1]/td[2]/input"));
-        FirstNameElement.sendKeys("David");
+        FirstNameElement.sendKeys(FirstName);
 
         WebElement LastNameElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[2]/td[2]/input"));
-        LastNameElement.sendKeys("Peter");
+        LastNameElement.sendKeys(LastName);
 
         WebElement EmailElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[3]/td[2]/input"));
-        EmailElement.sendKeys("collins.einstein@gmail.com");
+        EmailElement.sendKeys(Email);
 
         WebElement PhoneElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[4]/td[2]/input"));
-        PhoneElement.sendKeys("0987654321");
+        PhoneElement.sendKeys(Phone);
 
         WebElement Address1Element = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[5]/td[2]/input"));
-        Address1Element.sendKeys("DubaiKurukuSandu");
+        Address1Element.sendKeys(Address1);
 
         WebElement Address2Element = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[6]/td[2]/input"));
-        Address2Element.sendKeys("DubaiMainRoad");
+        Address2Element.sendKeys(Address2);
 
         WebElement CityElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[7]/td[2]/input"));
-        CityElement.sendKeys("Chennai");
+        CityElement.sendKeys(City);
 
         WebElement StateElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[8]/td[2]/input"));
-        StateElement.sendKeys("TamilNadu");
+        StateElement.sendKeys(State);
 
         WebElement ZipElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[9]/td[2]/input"));
-        ZipElement.sendKeys("600900");
+        ZipElement.sendKeys(Zip);
 
         WebElement CountryElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[2]/tbody/tr[10]/td[2]/input"));
-        CountryElement.sendKeys("India");
+        CountryElement.sendKeys(Country);
 
         but.stopTheFlow(3);
-    }
-
-    @Test(priority = 7)
-    public void ProfileInformation() {
 
         log.info("Step 7: Displaying Profile Information");
-        Select Language = new Select(driver.findElement(By
+        Select language = new Select(driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[3]/tbody/tr[1]/td[2]/select")));
-        Language.selectByValue("english");
+        language.selectByValue(Language);
 
-        Select Category = new Select(driver.findElement(By
+        Select category = new Select(driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[3]/tbody/tr[2]/td[2]/select")));
-        Category.selectByValue("FISH");
+        category.selectByValue(Category);
 
         WebElement EnableMyListElement = driver.findElement(By
                 .xpath("//*[@id=\"Catalog\"]/form/table[3]/tbody/tr[3]/td[2]/input"));
@@ -175,5 +142,26 @@ public class JetPetDataProviderTest extends BaseUtilTest {
         SaveAccountInformationElement.click();
 
         but.stopTheFlow(3);
+    }
+
+    @DataProvider
+    public Object[][] JetPetLoginData() {
+        Object[][] data = new Object[1][15];
+        data[0][0] = "Vincent";
+        data[0][1] = "V@21*89";
+        data[0][2] = "V@21*89";
+        data[0][3] = "Vincent";
+        data[0][4] = "Kumar";
+        data[0][5] = "Vinc@gmail.com";
+        data[0][6] = "2345612901";
+        data[0][7] = "abghstreet";
+        data[0][8] = "sdfgstreet";
+        data[0][9] = "Ooty";
+        data[0][10] = "TamilNadu";
+        data[0][11] = "703100";
+        data[0][12] = "India";
+        data[0][13] = "japanese";
+        data[0][14] = "DOGS";
+        return data;
     }
 }
