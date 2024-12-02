041ecadaef10c26072d60fa318e67be92bc0b53b
stephania-ezra <175607859+stephania-ezra@users.noreply.github.com>
2024-12-02 08:30:00 +0530

    interview questions

--- src/main/java/interview/program/Swap.java
+++ src/main/java/interview/program/Swap.java
@@ -18,13 +18,33 @@ public class Swap {
         a = b;
         b = temp;
          log.info("After Swapping a is:{}\n b is:{}", a, b);*/
-        
+
         // logic 2 : using + and - operator , without using temp variable
-        a = a + b; //a = 10+20 = 30
+        /*a = a + b; //a = 10+20 = 30
         b = a - b; // b = 30 -20 = 10
-        a = a - b; // a = 30 -10 = 20
+        a = a - b; // a = 30 -10 = 20*/
 
+        //log.info("After Swapping a is:{}\n b is:{}", a, b);
+
+        // logic 3 : using * and / operator , without using temp variable
+
+        /*a = a * b; //a = 10*20 = 200
+        b = a / b; // b = 200/10 = 20
+        a = a / b; // a = 200/20 = 10
+
+        log.info("After Swapping a is:{}\n b is:{}", a, b);*/
+
+        //logic 4 : using ^ operator
+
+       /* a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        log.info("After Swapping a is:{}\n b is:{}", a, b);*/
+
+        // 5. Logic 5 : Single Statement
+
+        b = a + b - (a = b);
         log.info("After Swapping a is:{}\n b is:{}", a, b);
-
     }
 }
