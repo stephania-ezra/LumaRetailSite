6d78780250d1dc8b84414f4ccdbb1ac5477a7b31
stephania <175607859+stephania-ezra@users.noreply.github.com>
2024-12-05 08:23:50 +0530

    String related programs - hashmap

---
+++ src/main/java/interview/program/stringprograms/CharCount.java
@@ -0,0 +1,35 @@
+package interview.program.stringprograms;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+
+/**
+ * Program to calculate the occurences of each letter
+ * naveen automation lab - youtube video
+ */
+public class CharCount {
+    public static Logger log = LogManager.getLogger(CharCount.class);
+
+    HashMap<Character,Integer> charMap = new HashMap<Character,Integer>();
+
+    String s = " ";
+
+    void getCharCount(){
+     char[] strArray = s.toCharArray();
+     for ( char c : strArray){
+         if (charMap.containsKey(c)){
+             charMap.put(c, charMap.get(c)+1);
+         }else{
+             charMap.put(c,1);
+         }
+     }
+     log.info(charMap);
+    }
+    public static void main(String[] args) {
+        CharCount cc = new CharCount();
+        log.info("The given String is:{}", cc.s);
+        cc.getCharCount();
+    }
+}
--- src/main/java/interview/program/stringprograms/OccuranceLearn.java
+++ src/main/java/interview/program/stringprograms/OccuranceLearn.java
@@ -5,8 +5,14 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 
-public class OccurenceLearn {
-    public static Logger log = LogManager.getLogger(OccurenceLearn.class);
+/**
+ * Program to count the occurrence by using for loop
+ * getting the integer at a specified position
+ * if the integer already available increase the count
+ * else mark the count as 1
+ */
+public class OccuranceLearn{
+    public static Logger log = LogManager.getLogger(OccuranceLearn.class);
 
     public static void main(String[] args) {
 
@@ -14,7 +20,8 @@ public class OccurenceLearn {
         //str.length()-1 = 3-1 =2
         HashMap<Character,Integer> charCount = new HashMap<Character,Integer>();
 
-       for ( int i = str.length()-1 ; i >=0 ; i --){
+      // for ( int i = str.length()-1 ; i >=0 ; i --){
+        for ( int i = 0 ; i <= str.length()-1 ; i ++){
            if (charCount.containsKey(str.charAt(i))){
                int count = charCount.get(str.charAt(i));
                charCount.put(str.charAt(i), ++count);
@@ -26,9 +33,13 @@ public class OccurenceLearn {
     }
 }
 /** for ( int i = 2 ; i >= 0 ; i --)
- *  str.charAt(2 ) = o ; if ( charCount.str.charAt(2)
+ *  str.charAt(2 ) = o ; if ( charCount.str.charAt(2)){
+ *      else
+ *      0 2
+ *      z 1
+ *
+ *  }
  *
  *
  *
  */
-/
